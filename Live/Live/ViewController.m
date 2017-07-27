35f7c4787f0543862ed24d2d27752f92eb1a7deb
zhu zhongshen <lazyrevengekingcat@gmail.com>
2017-07-27 11:31:41 +0800

    Live直播
    Live直播

---
+++ Live/Live/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Live
+//
+//  Created by goldeneye on 2017/7/27.
+//  Copyright © 2017年 goldeneye by smart-small. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
